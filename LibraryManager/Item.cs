c877aa5eb88b702d808343a775db176cdea359d0
Jeremy Osborn <josborn@sbts.edu>
2021-06-14 13:42:18 -0400

    Add project files.

---
+++ LibraryManager/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager
+{
+    class Item
+    {
+        public string Title { get; set; }
+        public bool OnLoan { get; set; }
+        public string Loanee { get; set; }
+    }
+}
---
+++ LibraryManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace LibraryManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+            var file = new FileInfo(fileName);
+            if (file.Exists)
+            {
+                using var reader = new StreamReader(file.FullName);
+                Console.SetIn(reader);
+                Console.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+}
